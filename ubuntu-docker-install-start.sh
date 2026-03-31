325a0220209fd0577900112b38d655e8ef3d6221
littlexia4-creator <littlexia4@gmail.com>
2026-04-01 01:31:43 +0800

    update ubuntu-docker-install-start.sh

--- ubuntu-docker-install-start.sh
+++ ubuntu-docker-install-start.sh
@@ -1,6 +1,6 @@
 # Add Docker's official GPG key:
 sudo apt update
-sudo apt install ca-certificates curl
+sudo apt install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -16,7 +16,7 @@ EOF
 
 sudo apt update
 
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 sudo systemctl enable docker
 sudo systemctl start docker
